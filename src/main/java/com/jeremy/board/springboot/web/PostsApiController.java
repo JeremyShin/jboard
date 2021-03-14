8dc8638dd7606fc9dc465d551971e893741c16e0
jeremyshin <younchul.shin@gmail.com>
2021-03-15 00:13:40 +0900

    게시판 조회, 등록, 업데이트 기능 (api)

--- src/main/java/com/jeremy/board/springboot/web/PostsApiController.java
+++ src/main/java/com/jeremy/board/springboot/web/PostsApiController.java
@@ -1,4 +1,29 @@
 package com.jeremy.board.springboot.web;
 
+import com.jeremy.board.springboot.service.posts.PostsService;
+import com.jeremy.board.springboot.web.dto.PostsResponseDto;
+import com.jeremy.board.springboot.web.dto.PostsSaveRequestDto;
+import com.jeremy.board.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
 public class PostsApiController {
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long Save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
