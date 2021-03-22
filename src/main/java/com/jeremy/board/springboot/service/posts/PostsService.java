dcc54bd49eed7af53125bbf23603a860b949fcad
jeremyshin <younchul.shin@gmail.com>
2021-03-22 21:35:51 +0900

    게시글 등록

--- src/main/java/com/jeremy/board/springboot/service/posts/PostsService.java
+++ src/main/java/com/jeremy/board/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.jeremy.board.springboot.service.posts;
 
 import com.jeremy.board.springboot.domain.posts.Posts;
 import com.jeremy.board.springboot.domain.posts.PostsRepository;
+import com.jeremy.board.springboot.web.dto.PostsListResponseDto;
 import com.jeremy.board.springboot.web.dto.PostsResponseDto;
 import com.jeremy.board.springboot.web.dto.PostsSaveRequestDto;
 import com.jeremy.board.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,10 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id+"+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
